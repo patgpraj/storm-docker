37f1475175f251c19a9b76e1d6ff80c3114e75fb
pratik <pratik@steelhouse.com>
2017-04-12 15:05:35 -0700

    docker registry update

--- client.sh
+++ client.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -i -t --link stormdocker_nimbus_1:nimbus  -p 22 registry.steelhouse.com/storm /bin/bash
+docker run -i -t --link stormdocker_nimbus_1:nimbus  -p 22 steelhousedev/storm /bin/bash
--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#docker build -t="registry.steelhouse.com/ubuntu-java-gradle" base
-docker build -t="registry.steelhouse.com/storm:1.0.1" storm
-docker build -t="registry.steelhouse.com/storm-nimbus:1.0.1" storm-nimbus
-docker build -t="registry.steelhouse.com/storm-supervisor:1.0.1" storm-supervisor
-docker build -t="registry.steelhouse.com/storm-ui:1.0.1" storm-ui
+docker build -t="steelhousedev/ubuntu-java-gradle" base
+docker build -t="steelhousedev/storm:1.0.1" storm
+docker build -t="steelhousedev/storm-nimbus:1.0.1" storm-nimbus
+docker build -t="steelhousedev/storm-supervisor:1.0.1" storm-supervisor
+docker build -t="steelhousedev/storm-ui:1.0.1" storm-ui
